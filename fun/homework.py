5abce23a6cd9a0478396ac3ee9c8dcf132d3982f
vcu-nkim <78060779+vcu-nkim@users.noreply.github.com>
2021-03-07 18:30:36 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -36,12 +36,10 @@ def longest_value_key(incoming_dict):
     Required parameter, incoming_dict, should be a dict.
     Find the KEY that has a value with the highest length, use the len() function
     """
-    test_list = [{'Gfg' :  "abcd", 'best' : 2},  
-             {'Gfg' :  "qwertyui", 'best' : 2}, 
-             {'Gfg' :  "xcvz", 'good' : 3}, 
-             {'Gfg' : None, 'good' : 4}] 
-    print("The original list is : " + str(test_list))
-    filt_key = 'Gfg'
-    temp = (sub[filt_key] for sub in test_list) 
-    res = max(len(ele) for ele in temp if ele is not None)
-    print("The maximum length key value : " + str(res))
+    result = 0
+    max = 0
+    for key in incoming_dict.keys():
+        if len(incoming_dict[key]) > max:
+            max = len(incoming_dict[key])
+            result = key
+    return result
