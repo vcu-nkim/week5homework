40fa7d0b76a9d39393309d2016231755b9e1ccfa
vcu-nkim <78060779+vcu-nkim@users.noreply.github.com>
2021-03-07 17:43:45 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -6,8 +6,8 @@ def find_greatest_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the largest number in the list.
     """
-    incoming_list = [456, 700, 200]
-    print("Max value element : ", max(incoming_list))
+    greatest = max(incoming_list)
+    return greatest
     
 
 def find_least_number(incoming_list):
@@ -15,8 +15,8 @@ def find_least_number(incoming_list):
     Required parameter, incoming_list, should be a list.
     Find the smallest/least number in the list.
     """
-    incoming_list = [456, 700, 200]
-    print("Min value element : ", min(incoming_list))
+    least = min(incoming_list)
+    return least
     
     
 def add_list_numbers(incoming_list):
