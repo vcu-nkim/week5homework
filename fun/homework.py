1c6f49f07eecc2aa22b851afb834cafd63f522d4
vcu-nkim <78060779+vcu-nkim@users.noreply.github.com>
2021-03-08 13:37:58 -0500

    Update homework.py

--- fun/homework.py
+++ fun/homework.py
@@ -2,41 +2,27 @@
 
 
 def find_greatest_number(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Find the largest number in the list.
-    """
     greatest = max(incoming_list)
     return greatest
-    
+    pass
+
 
 def find_least_number(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Find the smallest/least number in the list.
-    """
     least = min(incoming_list)
     return least
-    
+    pass
+
     
 def add_list_numbers(incoming_list):
-    """
-    Required parameter, incoming_list, should be a list.
-    Add all the values together and return it.
-    """
-    
     try:
         total = sum(incoming_list)
     except:
         total = 0
     return total
+    pass
 
     
 def longest_value_key(incoming_dict):
-    """
-    Required parameter, incoming_dict, should be a dict.
-    Find the KEY that has a value with the highest length, use the len() function
-    """
     try:
         result = None
         max = 0
@@ -47,3 +33,4 @@ def longest_value_key(incoming_dict):
     except:
         result = None
     return result
+    pass
